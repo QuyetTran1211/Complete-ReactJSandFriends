ed6ffc26a5b9676004ce4718eddcc71cbc2c5d1a
TranMinhQuyet <44421023+caigiha1@users.noreply.github.com>
2022-07-01 12:43:07 +0700

    form fields component how to create

--- exercise-files/forms/src/01-basic-button.js
+++ exercise-files/forms/src/01-basic-button.js
@@ -1,37 +1,26 @@
 import React from 'react';
-
-const content = document.createElement('div');
-document.body.appendChild(content);
-
+/* eslint-disable */
 module.exports = class extends React.Component {
-  static displayName = "01-basic-button";
+    static displayName = '01-basic-button';
 
-  onButtonClick = (evt) => {
-    const btn = evt.target;
-    console.log(`The user clicked ${btn.name}: ${btn.value}`)
-  };
+    onGreatClick = (evt) => {
+        console.log('The user clicked button-1: great', evt);
+    };
 
-  render() {
-    return (
-      <div>
-        <h1>What do you think of React?</h1>
+    onAmazingClick = (evt) => {
+        console.log('The user clicked button-2: amazing', evt);
+    };
 
-        <button
-          name='button-1'
-          value='great'
-          onClick={this.onButtonClick}
-        >
-          Great
-        </button>
+    render() {
+        <div>
+            <h1>What do you think of React</h1>
 
-        <button
-          name='button-2'
-          value='amazing'
-          onClick={this.onButtonClick}
-        >
-          Amazing
-        </button>
-      </div>
-    );
-  }
+            <button name="button-1" value="great" onClick={this.onGreatClick}>
+                Great
+            </button>
+            <button name="button-2" value="great" onClick={this.onAmazingClick}>
+                Amazing
+            </button>
+        </div>;
+    }
 };
--- exercise-files/forms/src/index.js
+++ exercise-files/forms/src/index.js
@@ -16,9 +16,17 @@ import Ten from './10-remote-persist.js';
 import Eleven from './11-redux-app.js';
 
 const routes = [
-  One, Two, Three, Four, Five,
-  Six, Seven, Eight, Nine,
-  Ten, Eleven,
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Eleven
 ];
 
 const location = window.location;
@@ -32,40 +40,62 @@ document.body.appendChild(container);
 render(element, container);
 
 function createTOC() {
-  return (
-    <div className='ui container'>
-      <h1>React Forms</h1>
-      <ul className='ui list'>
-        {routes.map((route, i) =>
-          <li key={i}><a href={`#/${(i + 1)}`}>{route.displayName}</a></li>
-        )}
-      </ul>
-    </div>
-  );
+    return (
+        <div className="ui container">
+            <h1>React Forms</h1>
+            <ul className="ui list">
+                {routes.map((route, i) => (
+                    <li key={i}>
+                        <a href={`#/${i + 1}`}>{route.displayName}</a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 function createRoute(nStr) {
-  let i = nStr - 1;
-  if (i < 0) i = 0;
-  if (i > routes.length - 1) i = routes.length - 1;
+    let i = nStr - 1;
+    if (i < 0) i = 0;
+    if (i > routes.length - 1) i = routes.length - 1;
 
-  const navStyle = {
-    position: 'fixed', bottom: 50, textAlign: 'center', left: '10%'
-  };
-  const isFirst = i <= 0;
-  const isLast = i >= routes.length - 1;
+    const navStyle = {
+        position: 'fixed',
+        bottom: 50,
+        textAlign: 'center',
+        left: '10%'
+    };
+    const isFirst = i <= 0;
+    const isLast = i >= routes.length - 1;
 
-  return (
-    <div className="ui container">
-      {React.createElement(routes[i])}
-      <div style={navStyle} 
-           className='ui fluid three item pagination menu'>
-        { isFirst ? '' : <a href={`#/${i}`} className='item'>{'<'}</a> }
-        <a href={location.href.replace(location.hash, '')}
-           className='item'
-          >TOC</a>
-        { isLast ? '' : <a href={`#/${(i + 2)}`} className='item'>{' > '}</a> }
-      </div>
-    </div>
-  );
+    return (
+        <div className="ui container">
+            {React.createElement(routes[i])}
+            <div
+                style={navStyle}
+                className="ui fluid three item pagination menu"
+            >
+                {isFirst ? (
+                    ''
+                ) : (
+                    <a href={`#/${i}`} className="item">
+                        {'<'}
+                    </a>
+                )}
+                <a
+                    href={location.href.replace(location.hash, '')}
+                    className="item"
+                >
+                    TOC
+                </a>
+                {isLast ? (
+                    ''
+                ) : (
+                    <a href={`#/${i + 2}`} className="item">
+                        {' > '}
+                    </a>
+                )}
+            </div>
+        </div>
+    );
 }
